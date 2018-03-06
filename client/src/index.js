1f56e966f26b743917c0cefd98e0dcb8b0d86e9f
David Darley <daviddar232@gmail.com>
2018-03-06 11:41:20 -0800

    [fix] - removing webpack from dependencies

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Render } from 'react-dom';
+import { render } from 'react-dom';
 import App from './Components/App';
 
 render(<App />, document.getElementById('app'));
