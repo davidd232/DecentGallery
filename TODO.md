ef00fd5d87175f6ce47a09d7b5e505066839ceff
David Darley <daviddar232@gmail.com>
2018-03-06 13:43:13 -0800

    [add] - adding a login comp in ownerComps for Kaleb to use in the future to add routes.

--- TODO.md
+++ TODO.md
@@ -1,3 +1,3 @@
 [] - ADD Owner only route to {siteName}/owner that redirects to home page with owner only views.
 [X] - Get basic route structure set up.
-[] - 
+[X] - Basic Styling to get an idea for the look.
---
+++ client/src/Components/OwnerComps/Login.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Decent Gallery is Decent.</h1>
+      </div>
+    )
+  }
+}
+
+export default Login;
