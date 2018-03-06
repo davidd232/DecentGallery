0e2b70c031be351d2db2256005f9eb928e2dab1a
David Darley <daviddar232@gmail.com>
2018-03-06 11:26:27 -0800

    [init] - initial commit with a bunch of great code to get started on this galleries website.

---
+++ server/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.resolve(__dirname, '../client/public')));
+
+app.listen(3000, () => {
+  console.log('app listening on 3000');
+})
