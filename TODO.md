d8d7c838b43b831c148bb55eac323844c508d218
David Darley <daviddar232@gmail.com>
2018-03-13 19:44:38 -0700

    [edit] - messed with todos to move to complete.

--- TODO.md
+++ TODO.md
@@ -2,7 +2,7 @@
 [] - Add backend routes for owner.
 [] - Finish basic login token handling.
 [] - Add bootstrap to get styling even through whole site because of laziness and lack of time.
-[X] - Add token to owner's local storage on successful login.
+
 
 
 
@@ -12,3 +12,4 @@
 COMPLETE:
 [X] - Get basic route structure set up.
 [X] - Basic Styling to get an idea for the look.
+[X] - Add token to owner's local storage on successful login.
--- client/src/Components/Nav/Header.jsx
+++ client/src/Components/Nav/Header.jsx
@@ -13,16 +13,24 @@ class Header extends Component {
         <div className='companyName'>
           <img onClick={() => { this.props.history.push('/') }} src='./images/DecentLetters.png' alt='name' />
         </div>
-        <div className='spans'>
-          <span onClick={() => { this.props.history.push('/') }}>Home</span>
-          <span onClick={() => { this.props.history.push('/shows') }}>Shows</span>
-          <span onClick={() => { this.props.history.push('/media') }}>Media</span>
-          <span onClick={() => { this.props.history.push('/shop') }}>Shop</span>
-          <span onClick={() => { this.props.history.push('/about') }}>About</span>
-        </div>
+
         {localStorage.getItem('token') ?
+          <div className='spans'>
+            <span onClick={() => { this.props.history.push('/') }}>Home</span>
+            <span onClick={() => { this.props.history.push('/shows') }}>Shows</span>
+            <span onClick={() => { this.props.history.push('/media') }}>Media</span>
+            <span onClick={() => { this.props.history.push('/shop') }}>Shop</span>
+            <span onClick={() => { this.props.history.push('/about') }}>About</span>
+          </div>
           <button onClick={this.handleClick.bind(this)}>Logout</button>
           :
+          <div className='spans'>
+            <span onClick={() => { this.props.history.push('/') }}>Home</span>
+            <span onClick={() => { this.props.history.push('/shows') }}>Shows</span>
+            <span onClick={() => { this.props.history.push('/media') }}>Media</span>
+            <span onClick={() => { this.props.history.push('/shop') }}>Shop</span>
+            <span onClick={() => { this.props.history.push('/about') }}>About</span>
+          </div>
           <img onClick={() => { this.props.history.push('/owner') }} className='companyFace' src='./images/DecentHead.png' alt='logoHead' />
         }
       </div>
